3e29ae9973b2b24a234911abf4e7bd09b92b6ce5
Denis Seligin <seligindenis@yandex.ru>
2024-03-27 11:05:26 +0300

    animation

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,19 +1,22 @@
 package ru.netology.statsview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.stats_view).data = listOf(
-            0.2F,
-            0.2F,
-            0.2F,
-            0.2F,
-            0.2F,
-        )
+        val view = findViewById<StatsView>(R.id.stats_view)
+        view.postDelayed({
+            view.data = listOf(
+                0.2F,
+                0.2F,
+                0.2F,
+                0.2F,
+                0.2F,
+            )
+        }, 50)
     }
 }
